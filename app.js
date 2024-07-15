b9f5fa077fb0838f67c016c1ffba1e708f65034d
JessR90 <jessica.roias@yahoo.com>
2024-07-15 16:34:14 -0400

    update problems

--- app.js
+++ app.js
@@ -231,7 +231,9 @@ After writing this method, call it and pass in a Pokemon object of your choice f
 
 Solve Exercise 10 here:
 */
-
+game.catchPokemon = function(pokemonObj) {
+  catchPokemon.push(pokemonObj)
+}
 
 
 
@@ -317,6 +319,7 @@ console.log(game.gyms);
 Exercise 16
 1. Log the entire `game` object to the console. Take a moment to review the changes you've made throughout the exercises.
 
-
 Solve Exercise 16 here:
 */
+
+console.log(game);
